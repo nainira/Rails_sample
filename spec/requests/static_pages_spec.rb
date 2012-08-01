24e6a2ce0cb90ef5f76683e89d4b5f6f96e53ae6
Ryan Kim <nesteriajik@gmail.com>
2012-08-01 16:13:41 +0900

    excercises finish and configure postgre

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,10 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  # describe "GET /static_pages" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get static_pages_index_path
-  #     response.status.should be(200)
-  #   end
-  # end
 
-  describe "Home page" do
+    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+    describe "Home page" do
   	
   	it "should have the content 'Sample App'" do
   		visit '/static_pages/home'
@@ -18,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', text: "#{base_title} | Home")
     end
     
   end
@@ -31,7 +26,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', text: "#{base_title} | Help")
     end
 
   end
@@ -44,8 +39,21 @@ describe "StaticPages" do
 
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | About")
+      page.should have_selector('title', text: "#{base_title} | About")
     end
 
+  end
+
+  describe "Contact page" do
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "#{base_title} | Contact")
+    end
   end
 end
