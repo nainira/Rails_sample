10dcedc321c0a599848ba3c5e115c158367d3b24
Ryan Kim <nesteriajik@gmail.com>
2012-09-20 19:10:32 +0900

    done til 10.3.2 Creating microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,12 +1,18 @@
 class MicropostsController < ApplicationController
-	before_filter :signed_in_user, only: [:create, :destroy]
+	before_filter :signed_in_user
 
 	def index
 		
 	end
-	
+
 	def create
-		
+		@micropost = current_user.microposts.build(params[:micropost])
+		if @micropost.save
+			flash[:success] = "Micropost created!"
+			redirect_to root_url
+		else
+			render 'static_pages/home'
+		end
 	end
 
 	def destroy
