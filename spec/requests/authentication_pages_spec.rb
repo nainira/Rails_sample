735012482ce03bf29899140bf6c2f772f3f068af
Ryan Kim <nesteriajik@gmail.com>
2012-10-11 18:23:38 +0900

    done til 11.2.3 Following and followers pages

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -91,6 +91,16 @@ describe "AuthenticationPages" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+        describe "visiting the following page" do
+          before {  visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in')}
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
 
       describe "in the Microposts controller" do
